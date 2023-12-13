11144bfade8d4ada96f7bb4488a16ba07b03a13a
Dhruvp132 <dhruvpatel1321@gmail.com>
2023-12-13 14:07:27 +0530

    Updates

---
+++ precache-manifest.a4937ba5ab8cc9efe23eaffc90922385.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "55fb82bb4486c5c2c235",
+    "url": "/NewsApp/static/css/main.ef27cc53.chunk.css"
+  },
+  {
+    "revision": "55fb82bb4486c5c2c235",
+    "url": "/NewsApp/static/js/main.55fb82bb.chunk.js"
+  },
+  {
+    "revision": "b5dec1235d8d1d118e4f",
+    "url": "/NewsApp/static/js/1.b5dec123.chunk.js"
+  },
+  {
+    "revision": "d94d88b2a23a16a5ac26",
+    "url": "/NewsApp/static/js/2.d94d88b2.chunk.js"
+  },
+  {
+    "revision": "075c807d0a04bf7476e6",
+    "url": "/NewsApp/static/js/runtime~main.075c807d.js"
+  },
+  {
+    "revision": "eba5cf1f057edf064a324f60f66bc67b",
+    "url": "/NewsApp/static/media/loading.eba5cf1f.gif"
+  },
+  {
+    "revision": "54f670a9b7a58ce479f3a4c676f6be54",
+    "url": "/NewsApp/index.html"
+  }
+];
